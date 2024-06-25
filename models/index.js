9418fc8276c11eac773add850377eb979c520b92
hanheasung <hanhasung07@gmail.com>
2024-06-25 15:35:09 +0900

    nodebird 데이터베이스 세팅 완료

--- models/index.js
+++ models/index.js
@@ -1,9 +1,11 @@
-const Sequelize = require('sequelize');
-const env = process.env.NODE_ENV || 'development';
-const fs = require('fs');
-const path = require('path');
-const config = require('../config/config')[env];
-
+const Sequelize = require('sequelize'); // Sequelize 모듈 불러오기
+const path = require('path'); // path 모듈 가져오기
+const fs = require('fs'); // 파일 시스템 모듈 가져오기
+const User = require('./user'); // User 모델 불러오기
+const Post = require('./post'); // Post 모델 불러오기
+const Hashtag = require('./hashtag'); // Hashtag 모델 불러오기
+const env = process.env.NODE_ENV || 'development'; // 현재 환경 설정
+const config = require('../config/config')[env];  // 환경 설정 파일 불러오기
 const db = {};
 const sequelize = new Sequelize(
   config.database, config.username, config.password, config,
